cc3a4b80d7f22b8eeb8cc22d4da092e325c64331
Zakhar Kutsko <kutskolukes@gmail.com>
2022-08-21 11:29:50 +0300

    add PropTypes to Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,42 +1,45 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import "./style.css";
 
-const Form = () => {
-  return (
-    <form className="form">
-      <input
-        required={true}
-        className="form__input"
-        type="text"
-        name="name"
-        placeholder="name"
-      />
-      <input
-        required={true}
-        className="form__input"
-        type="text"
-        placeholder="surname"
-        name="surname"
-      />
-      <input
-        required={true}
-        className="form__input"
-        type="email"
-        name="email"
-        placeholder="email"
-      />
-      <input
-        required={true}
-        className="form__input"
-        type="date"
-        name="birthday"
-      />
-      <button className="form__button" type="submit">
-        Save
-      </button>
-    </form>
-  );
-};
+const Form = () => (
+  <form className="form">
+    <input
+      required={true}
+      className="form__input"
+      type="text"
+      name="name"
+      placeholder="name"
+    />
+    <input
+      required={true}
+      className="form__input"
+      type="text"
+      placeholder="surname"
+      name="surname"
+    />
+    <input
+      required={true}
+      className="form__input"
+      type="email"
+      name="email"
+      placeholder="email"
+    />
+    <input
+      required={true}
+      className="form__input"
+      type="date"
+      name="birthday"
+    />
+    <button className="form__button" type="submit">
+      Save
+    </button>
+  </form>
+);
+
+Form.propTypes = {};
+
+Form.defaultProps = {};
 
 export default Form;
